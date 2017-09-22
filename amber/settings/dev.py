e4d7921ac85309fcce81668795d633caabbd9b43
Taylor C. Richberger <Taywee@gmx.com>
2017-09-21 20:48:06 -0600

    initialize wagtail

---
+++ amber/settings/dev.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import, unicode_literals
+
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '+r4_0_f^2gz2l5%6hv8bp=!e2v@h)@1^_ihpps5131ib0)w46$'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
