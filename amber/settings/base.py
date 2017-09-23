9084c0ad6e4df682d1971ca637f98938b273f930
Taylor C. Richberger <Taywee@gmx.com>
2017-09-22 21:58:18 -0600

    add news and QOL extensions

--- amber/settings/base.py
+++ amber/settings/base.py
@@ -50,6 +50,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'wagtailnews',
+    'wagtail.contrib.wagtailroutablepage',
 ]
 
 MIDDLEWARE = [
